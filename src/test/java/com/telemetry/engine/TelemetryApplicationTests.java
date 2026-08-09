82088feb427f357c19fbb44671ebc36705eb9473
Josiah Christian <christianjosiah2484@gmail.com>
2026-08-09 15:04:01 -0400

    Add Spring Boot application context test

---
+++ src/test/java/com/telemetry/engine/TelemetryApplicationTests.java
@@ -0,0 +1,12 @@
+package com.telemetry.engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelemetryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
